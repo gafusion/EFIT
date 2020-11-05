5e69efc6efcca2c53575e8a65ce1b2b47917dc25
Joseph McClenaghan <jmcclena@gmail.com>
2020-11-05 14:39:12 -0800

    change afile to max difference too

--- testing/compare_eqdsks.py
+++ testing/compare_eqdsks.py
@@ -12,19 +12,20 @@ def compare_afiles(time):
     aname = 'a' + str(shot).zfill(6) +'.'+ str(time).zfill(5)
     items = ['ali3', 'bcentr','betan', 'cpasma', 'doutl', 'sibdry','simagx','wplasm', 'terror']
     print ('shot: ', shot, 'time ', time)
-    serror_all = 0.
-    perror_all = 0.
+    serror_imax = (0.,'','')
+    perror_imax = (0.,'','')
     for item in items:
-        perror = abs((dirs[0][aname][item] - dirs[1][aname][item])/dirs[0][aname][item])
-        print(item,'(parall): ', dirs[0][aname][item], dirs[1][aname][item], perror)
-        serror = abs((dirs[2][aname][item] - dirs[3][aname][item])/dirs[3][aname][item])
-        print(item,'(serial): ', dirs[2][aname][item], dirs[3][aname][item], serror)
-        serror_all += serror
-        perror_all += perror
-    serror /= len(items)
-    perror /= len(items)
+        perror = (abs((dirs[0][aname][item] - dirs[1][aname][item])/dirs[0][aname][item]), item, aname)
+        print(item,'(parall): ', dirs[0][aname][item], dirs[1][aname][item], perror[0])
+        serror = (abs((dirs[2][aname][item] - dirs[3][aname][item])/dirs[3][aname][item]), item, aname)
+        print(item,'(serial): ', dirs[2][aname][item], dirs[3][aname][item], serror[0])
+        if serror[0] > serror_imax[0]:
+            serror_imax = serror
+        if perror[0] > perror_imax[0]:
+            perror_imax = perror
+
     print ('\n')
-    return serror, perror
+    return serror_max, perror_imax
 
 def compare_gfiles(time):
     fig = plt.figure()
@@ -35,24 +36,39 @@ def compare_gfiles(time):
 
     nw = len(dirs[0][gname]['PSIRZ'])
     nh = len(dirs[0][gname]['PSIRZ'][0,:])
-    R = numpy.linspace(dirs[0][gname]['RLEFT'], dirs[0][gname]['RDIM'], nw)              
-    Z = numpy.linspace(dirs[0][gname]['ZMID']-dirs[0][gname]['ZDIM'], dirs[0][gname]['ZMID']-dirs[0][gname]['ZDIM'], nw)
+    R = numpy.linspace(dirs[0][gname]['RLEFT'], dirs[0][gname]['RLEFT']+dirs[0][gname]['RDIM'], nw)              
+    Z = numpy.linspace(dirs[0][gname]['ZMID']-dirs[0][gname]['ZDIM'], dirs[0][gname]['ZMID']+dirs[0][gname]['ZDIM'], nw)
     RR, ZZ = numpy.meshgrid(R, Z)
     dpsi1 = numpy.abs((dirs[1][gname]['PSIRZ'] - dirs[0][gname]['PSIRZ'])/dirs[0][gname]['PSIRZ'])
     dpsi2 = numpy.abs((dirs[3][gname]['PSIRZ'] - dirs[2][gname]['PSIRZ'])/dirs[2][gname]['PSIRZ'])
-    print(numpy.max(dpsi1),numpy.max(dpsi2))
-    fig, axs = plt.subplots(1,2)
-    fig.suptitle('Error is PSIRZ')
-    axs[0].contourf(RR, ZZ, dpsi1)
-    axs[1].contourf(RR, ZZ, dpsi2)
 
+    perror = (numpy.amax(dpsi1),numpy.unravel_index(dpsi1.argmax(), dpsi1.shape), gname)
+    serror = (numpy.amax(dpsi1),numpy.unravel_index(dpsi1.argmax(), dpsi1.shape), gname)
+
+    fig, axs = plt.subplots(1, 2, figsize=(8, 5))
+    fig.suptitle('Error is PSIRZ')
+    
+    cntr0 = axs[0].contourf(RR, ZZ, dpsi1)
+    cntr1 = axs[1].contourf(RR, ZZ, dpsi2)
+
+    axs[0].plot(dirs[0][gname]['RBBBS'], dirs[0][gname]['ZBBBS'])
+    axs[0].plot(dirs[1][gname]['RBBBS'], dirs[1][gname]['ZBBBS'])
+    axs[1].plot(dirs[2][gname]['RBBBS'], dirs[2][gname]['ZBBBS'])
+    axs[1].plot(dirs[3][gname]['RBBBS'], dirs[3][gname]['ZBBBS'])
+
+    axs[0].plot(dirs[0][gname]['RLIM'], dirs[2][gname]['ZLIM'],color='k')
+    axs[1].plot(dirs[0][gname]['RLIM'], dirs[3][gname]['ZLIM'],color='k')
+
+    fig.colorbar(cntr0, ax=axs[0])
+    fig.colorbar(cntr1, ax=axs[1])
+    
     axs[0].set_xlabel('R[m]')
     axs[0].set_ylabel('Z[m]')
     axs[1].set_xlabel('R[m]')
                        
-    fig.savefig(plotdir +'DPSI_' +gname +'.pdf')
-    perror = numpy.sum(dpsi1)/nw/nh
-    serror = numpy.sum(dpsi2)/nw/nh
+    fig.savefig(plotdir+'DPSI_'+gname +'.pdf')
+    # Return maximum error, indicies of maximum, and gfile name
+
     return serror, perror
 
 def compare_mfiles(time):
@@ -63,11 +79,13 @@ def compare_mfiles(time):
     fig.savefig(plotdir +mname+'.pdf')
     
     fig = plt.figure()
-    for mydir in dirs:
-        plt.semilogy(mydir[mname]['cerror']['data'][0,:])
+    labels = ['public parallel', 'new parallel','public serial','public serial']
+    for i, mydir in enumerate(dirs):
+        plt.semilogy(mydir[mname]['cerror']['data'][0,:], label=labels[i])
 
     plt.ylabel('chi^2')
     plt.xlabel('iteration')
+    plt.legend(loc='upper right')
     fig.savefig(plotdir +"CHI2_" + mname+'.pdf')
         
 
@@ -100,29 +118,36 @@ if os.path.exists(plotdir):
    os.system("rm -r "+plotdir)
 os.mkdir(plotdir)
 
-serror_all = 0.
-perror_all = 0.
+serror_max = (0., (0, 0), 'all gEQDSKs')
+perror_max = (0., (0, 0), 'all gEQDSKs')
 for time in all_times[0]:
     serror, perror = compare_gfiles(time)
-    serror_all += serror
-    perror_all += perror
+    if serror[0] > serror_max[0]:
+        serror_max = serror
+    if perror[0] > perror_max[0]:
+        perror_max = perror
+print (serror)
+print ("Maximum difference in gEQDSK PSI (serial) = {} at i={}, j={} for : {}".format(
+    serror_max[0], serror_max[1][0]+1, serror_max[1][1]+1, serror_max[2]))
+print ("Maximum difference in gEQDSK PSI (parallel) = {} at i={}, j={} for : {}".format(
+    perror_max[0], perror_max[1][0]+1, perror_max[1][1]+1, perror_max[2]))
 
-serror_all /= len(all_times[0])
-perror_all /= len(all_times[0])
-print ("Average total difference in PSI (serial) = ", serror_all)
-print ("Average total difference in PSI (parallel) = ", perror_all)
 
-serror_all = 0.
-perror_all = 0.
+serror_max = (0., 'all times', 'all aEQKSKs')
+perror_max = (0., 'all times', 'all aEQKSKs')
 for time in all_times[0]:
     serror, perror = compare_afiles(time)
-    serror_all += serror
-    perror_all += perror
+    if serror[0] > serror_max[0]:
+        serror_max = serror
+    if perror[0] > perror_max[0]:
+        perror_max = perror
 
-serror_all /= len(all_times[0])
-perror_all /= len(all_times[0])
-print ("Average total difference in aEQDSK (serial) = ", serror_all)
-print ("Average total difference in aEQDSK (parallel) = ", perror_all)
+
+print ("Maximum difference in aEQDSK (serial) = {} found in {} for {}".format(
+    serror_max[0], serror_max[1],  serror_max[2]))
+print ("Maximum difference in aEQDSK (parallel) = {} found in {} for {}".format(
+    perror_max[0], perror_max[1],  perror_max[2]))
+
 
 for time in all_times[0]:
     compare_mfiles(time)
--- testing/test_all.sh
+++ testing/test_all.sh
@@ -16,20 +16,20 @@ rm -r $shot
 echo "Testing EFIT01 for shot  $shot"
 ./test_snap.sh $shot "EFIT01" $efit_exe_pub $efit_exe > logs/test_EFIT01_$shot.log
 ./compare_eqdsks.sh "$shot/EFIT01" >> logs/test_EFIT01_$shot.log
-grep "Average total difference in" logs/test_EFIT01_$shot.log
+grep "Maximum difference in" logs/test_EFIT01_$shot.log
 echo ""
 
 echo "Testing EFIT02 for shot $shot"
 ./test_snap.sh $shot "EFIT02" $efit_exe_pub $efit_exe > logs/test_EFIT02_$shot.log
 ./compare_eqdsks.sh "$shot/EFIT02" >> logs/test_EFIT02_$shot.log
-grep "Average total difference in" logs/test_EFIT02_$shot.log
+grep "Maximum difference in" logs/test_EFIT02_$shot.log
 echo ""
 
 echo " Testing kfiles generation and gfile produced from kfiles for shot  $shot"
 ./test_kfiles.sh $shot 'EFIT01' $efit_exe_pub $efit_exe > logs/test_kfiles_$shot.log
 ./test_k_to_gfiles.sh $shot $efit_exe_pub $efit_exe > logs/test_k_to_gfiles_$shot.log
 ./compare_eqdsks.sh "$shot/kfiles" >> logs/test_k_to_gfiles_$shot.log
-grep "Average total difference in" logs/test_k_to_gfiles_$shot.log
+grep "Maximum difference in" logs/test_k_to_gfiles_$shot.log
 echo ""
 
 done
