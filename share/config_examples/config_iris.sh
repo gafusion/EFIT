4c42bf72fd3830ad4cfbac6c595c506f14d040da
Joseph McClenaghan <jmcclena@gmail.com>
2021-05-19 22:29:20 -0700

    update iris config

--- share/config_examples/config_iris.sh
+++ share/config_examples/config_iris.sh
@@ -1,8 +1,14 @@
-    #!/bin/tcsh
-    module load cmake
-    module load env/gcc9.2
+    /bin/rm -rf CMake*
 
     cmake \
+    -DENABLE_SHARED_LIBS:BOOL=TRUE \
+    -DCMAKE_C_COMPILER:FILEPATH='gcc' \
+    -DCMAKE_Fortran_COMPILER:FILEPATH='gfortran' \
+    -DCMAKE_C_FLAGS:STRING='-fvisibility=default -fPIC -pipe' \
+    -DCMAKE_Fortran_FLAGS:STRING='-fPIC -pipe' \
     -DHAVE_NETCDF:BOOL=TRUE \
-    -DNETCDF_DIR:PATH=/fusion/usc/opt/env/gcc9.2 \
+    -DNETCDF_DIR:PATH='/fusion/usc/opt/env/gcc9.2' \
+    -DBLAS_DIR:PATH='/fusion/usc/opt/env/gcc9.2' \
+    -DBLAS_LIBS:PATH='/fusion/usc/opt/env/gcc9.2' \
+    -DLAPACK_LIBRARIES:'PATH=/fusion/usc/opt/env/gcc9.2' \
     ..
