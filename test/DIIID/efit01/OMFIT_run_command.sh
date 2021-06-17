d41e993d3360cf03c55e1a826c6edf2377827443
Joseph McClenaghan <jmcclena@gmail.com>
2021-06-17 13:14:03 -0700

    Update efit01,efit02 test case to shot that is consistent with available mhdin.dat file add efit02er test case

--- test/DIIID/efit01/OMFIT_run_command.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/tcsh
-efit 65 << __c__MATCHING_EOF__c__
-2
-1
-k163303.02000
-
-__c__MATCHING_EOF__c__
--- test/DIIID/efit01/run_command.sh
+++ test/DIIID/efit01/run_command.sh
@@ -2,6 +2,6 @@
 /Users/kruger/efitairoot/efitall/efit-master/build/efit/efit 65 << __c__MATCHING_EOF__c__
 2
 1
-k163303.02000
+k186610.02300
 
 __c__MATCHING_EOF__c__
