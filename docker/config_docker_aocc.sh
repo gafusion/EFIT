c782d4a0ccf862fd0451607c85f38d649f431830
Scott Kruger <scott.e.kruger@gmail.com>
2021-12-06 10:47:02 -0700

    Updates to docker and CI testing
    - add aocc docker image because having flang would be useful
    - update docker documentation to reflect having more than 1 image
    - improve CI usage to make it less error prone and more transparent
    
    Currently cannot get aocc to work -- green/efund runs but efit does not.

---
+++ docker/config_docker_aocc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+dockerdir=`dirname $0`
+sourcedir=`dirname $dockerdir`
+
+rm -rf CMake*
+
+    cmake \
+        -DCMAKE_INSTALL_PREFIX:PATH=/software \
+        -DCMAKE_BUILD_TYPE:STRING=Debug \
+        -DCMAKE_COLOR_MAKEFILE:BOOL=TRUE \
+        -DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
+        -DTRAP_FP_EXCEPTIONS:BOOL=TRUE \
+        -DENABLE_OpenMP:BOOL=TRUE \
+        $sourcedir
---
+++ share/config_examples/config_ci_hip.sh
@@ -0,0 +1,7 @@
+# CI machine at hip.txcorp.com
+
+    cmake \
+        -DCMAKE_BUILD_TYPE:STRING=Debug \
+        -DCMAKE_COLOR_MAKEFILE:BOOL=TRUE \
+        -DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
+        ..
