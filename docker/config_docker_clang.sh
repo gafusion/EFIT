66282a3d2fd15a919503138124d26483a9b095cf
Scott Kruger <scott.e.kruger@gmail.com>
2022-01-17 14:27:13 -0700

    Enable artifacts, including those for docs
    Enable downloading log files from all the runs.
    
    Enable docs for the docker build.  This requires adding sphinx and
    plugins to docker image
    
    This also requires synchronization with Gitlab Pages

--- docker/config_docker_clang.sh
+++ docker/config_docker_clang.sh
@@ -10,4 +10,5 @@ rm -rf CMake*
         -DCMAKE_BUILD_TYPE:STRING=Debug \
         -DCMAKE_COLOR_MAKEFILE:BOOL=TRUE \
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=TRUE \
+        -DENABLE_DOCS:BOOL=TRUE \
         $sourcedir
