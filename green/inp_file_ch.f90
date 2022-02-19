9a3c74929070ef6f583c3f2996f14e913d630874
Torrin Bechtel <bechtelt@fusion.gat.com>
2022-02-19 00:23:45 -0800

    Some cleanup in green

---
+++ green/inp_file_ch.f90
@@ -0,0 +1,30 @@
+!**********************************************************************
+!>
+!!    This subroutine set the defines file name suffixes to look for
+!!    (e.g. if running nw=129 ,nw=129, efit will look for Green's function
+!!     tables with ec129129, re129129, etc.)
+!!    
+!!
+!!    @param nw : number of grid point (width)
+!!
+!!    @param nh : number of grid point (height)
+!!
+!!    @param ch1 : character of number of grid point (width)
+!!
+!!    @param ch2 : character of number of grid point (height)
+!!
+!**********************************************************************
+
+      subroutine inp_file_ch(nw,nh,ch1,ch2)
+        
+        integer*4,   intent(in)  :: nw, nh 
+        character*4, intent(out) :: ch1, ch2
+        
+        write(ch1,'(i4)') nw
+        ch1 = adjustl(ch1)
+        write(ch2,'(i4)') nh
+        ch2 = adjustl(ch2)
+        
+        return
+        
+      end subroutine inp_file_ch
