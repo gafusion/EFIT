c61f2daff80a897c0535bb1bd3bd36f8f16b44ff
bechtt <bechtelt@fusion.gat.com>
2023-06-19 11:12:27 -0700

    Enable NETCDF on hip
    It's unclear how tests were picking this up before, but it seems to
    not be linked any longer...

--- share/config_examples/config_ci_hip.sh
+++ share/config_examples/config_ci_hip.sh
@@ -14,9 +14,8 @@
         -DBLAS_blas_LIBRARY:FILEPATH=/scratch/soft/lib/libfblas.a \
         -DLAPACK_lapack_LIBRARY:FILEPATH=/scratch/soft/lib/libflapack.a \
         -DSphinx_EXECUTABLE:FILEPATH=/scratch/soft/miniconda3/bin/sphinx-build \
-        -DENABLE_HDF5:BOOL=TRUE \
+        -DENABLE_NETCDF:BOOL=ON \
+        -DENABLE_HDF5:BOOL=ON \
         -DENABLE_DOCS:BOOL=TRUE \
         -DHDF5_DIR:PATH=/scratch/soft/hdf5 \
         ..
-
-    # For future use:
