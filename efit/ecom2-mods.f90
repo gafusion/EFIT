gafusion/EFIT

commit 55535cc0255af951439c87e2dafdeff4c0234419

Add support for longer times and associated code clean-up

efit/print_terminal.F90
@@ -22,7 +22,7 @@
              dsi,nzz,saisq,sinow,sm1,sm2,ssiref, &
              sumif,sumifb,sumifs,sumift,tin,tout, &
              xdum,xnorm,xtest,ytest,xxm,yym,xxp,yyp
-      character*30 sfname
+      character*300 sfname
       character(1000) :: line
       real*8 xrsp(npcurn)
       real*8 patmpz(magpri),xmpz(magpri),ympz(magpri),ampz(magpri), &
@@ -417,9 +417,7 @@
         enddo
 !
         if (rank == 0) then
-          open(unit=80,status='old',file='mhdin.new',iostat=ioerr)
-          if (ioerr.eq.0) close(unit=80,status='delete')
-          open(unit=80,status='new',file='mhdin.new',delim='quote')
+          call open_new(80,'mhdin.new','','quote')
           write (80,in3)
           close(unit=80)
         endif
@@ -515,11 +513,8 @@
                   crmaxi(i),czmaxi(i),cemaxi(i),cqmaxi(i),cchisq(i)
       enddo
       if ((kwripre.gt.0).and.(kwripre.le.9)) then
-          call setfnmd('n',ishot,itime,sfname)
-          sfname=sfname(1:13)//'_error'
-          open(unit=74,status='old',file=sfname,iostat=ioerr)
-          if (ioerr.eq.0) close(unit=74,status='delete')
-          open(unit=74,status='new',file=sfname)
+          call setfnm('n',ishot,itime,itimeu,'_error',sfname)
+          call open_new(74,sfname,'','')
           do i=1,nitera
            write (74,*) i,cerror(i),xdum,xdum
           enddo

efit/set_defaults.f90
@@ -198,8 +198,8 @@
 !--------------------------------------------------------------------- 
       if ((kdata.eq.5).and.(kdata.eq.6)) then
         appendsnap='KG'
-        fwtcur=1.
-        fwtbp=1.
+        fwtcur=1.  ! TODO: why is this wanted?
+        fwtbp=1.  ! TODO: why is this wanted?
         iout=1                 ! default - write fitout.dat
         kffcur=3
         kppcur=3
@@ -425,12 +425,8 @@
         nw_ext=0
         nh_ext=0
         psin_ext(1)=-1000.0 
-        sign_ext=1.0
-        cratio_ext=1.0
         cratiop_ext=1.0
         cratiof_ext=1.0
-        scalepp_ext=1.0
-        scaleffp_ext=1.0
 
         cgama=0.0
         calpa=0.0

efit/tables.F90
@@ -43,7 +43,7 @@
 #if defined(USEMPI)
       include 'mpif.h'
 #endif
-      integer*4 :: i,reason,nFiles,itmp,imin
+      integer*4 :: i,reason,nFiles,itmp,imin,ltbdi2
       character(100) :: ftmp
       integer*4 :: shot_tables(100)
       character(100), dimension(:), allocatable :: filenames
@@ -54,17 +54,7 @@
         input_dir=trim(link_efit)
       endif
       if (link_store(1:1).ne.'')  store_dir=trim(link_store)
-
-      ! recalculate length of default directories in case any change
-      ltbdi2=0
-      lindir=0
-      lstdir=0
       table_di2 = table_dir
-      do i=1,len(table_di2)
-        if(table_di2(i:i).ne.' ') ltbdi2=ltbdi2+1
-        if(input_dir(i:i).ne.' ') lindir=lindir+1
-        if(store_dir(i:i).ne.' ') lstdir=lstdir+1
-      enddo
 
       if (rank==0) then
         ! get the files
@@ -100,10 +90,10 @@
         ! set the table directory
         do i=1, nfiles
           if(ishot.ge.shot_tables(i)) &
-            table_di2 = table_di2(1:ltbdi2)//trim(filenames(i))//'/'
+            table_di2 = trim(table_di2)//trim(filenames(i))//'/'
         enddo
         ltbdi2 = len(trim(table_di2))
-        write(nttyo,10000) table_di2(1:ltbdi2)
+        write(nttyo,10000) trim(table_di2)
       endif
 
 #if defined(USEMPI)
@@ -161,7 +151,7 @@
 !--   Read Green's tables from table_di2                            --
 !---------------------------------------------------------------------
       open(unit=mcontr,status='old',form='unformatted', &
-           file=table_di2(1:ltbdi2)//'ec'//trim(ch1)//trim(ch2)//'.ddd')
+           file=trim(table_di2)//'ec'//trim(ch1)//trim(ch2)//'.ddd')
       read (mcontr) mw,mh
 
       if (.not.allocated(rgrid)) then
@@ -190,7 +180,7 @@
 !--   read in the f coil response functions                          --
 !----------------------------------------------------------------------
       open(unit=mcontr,form='unformatted', &
-           status='old',file=table_di2(1:ltbdi2)//'rfcoil.ddd')
+           status='old',file=trim(table_di2)//'rfcoil.ddd')
       if (.not.allocated(rsilfc)) then
         allocate(rsilfc(nsilop,nfsum),stat=iallocate_stat)
         if(iallocate_stat/=0) stop "*** Not enough space for rsilfc ***"
@@ -205,7 +195,7 @@
       close(unit=mcontr)
 
       open(unit=mcontr,status='old',form='unformatted', &
-           file=table_di2(1:ltbdi2)//'ep'//trim(ch1)//trim(ch2)//'.ddd')
+           file=trim(table_di2)//'ep'//trim(ch1)//trim(ch2)//'.ddd')
       if (.not.allocated(gsilpc)) then
         allocate(gsilpc(nsilop,mw*mh),stat=iallocate_stat)
         if(iallocate_stat/=0) stop "*** Not enough space for gsilpc ***"
@@ -223,7 +213,7 @@
 !----------------------------------------------------------------------
       if (iecurr.gt.0) then
         open(unit=mcontr,status='old',form='unformatted', &
-          file=table_di2(1:ltbdi2)//'re'//trim(ch1)//trim(ch2)//'.ddd')
+          file=trim(table_di2)//'re'//trim(ch1)//trim(ch2)//'.ddd')
         read (mcontr) rsilec
         read (mcontr) rmp2ec
         read (mcontr) gridec
@@ -234,7 +224,7 @@
 !----------------------------------------------------------------------
       if (ifitvs.eq.1 .or. ivesel.gt.0) then
         open(unit=mcontr,status='old',form='unformatted', &
-           file=table_di2(1:ltbdi2)//'rv'//trim(ch1)//trim(ch2)//'.ddd')
+           file=trim(table_di2)//'rv'//trim(ch1)//trim(ch2)//'.ddd')
         read (mcontr) rsilvs
         read (mcontr) rmp2vs
         read (mcontr) gridvs
@@ -259,7 +249,7 @@
         endif
       endif
 
-      open(unit=mcontr,status='old',file=table_di2(1:ltbdi2)//'mhdin.dat')
+      open(unit=mcontr,status='old',file=trim(table_di2)//'mhdin.dat')
 
       read (mcontr,in3, iostat=istat)
       if (istat>0) then

efit/CMakeLists.txt
@@ -9,7 +9,7 @@ include_directories(
 set(EFITLIB_SOURCES
   modules-efit.F90 set_eparm.F90 psical.f90 mat_solve.f90 
   tables.F90 read_namelist.F90 set_defaults.f90
-  buneman.f90 set_filenames.f90 ecom1-mods.f90 zpline.f90 
+  buneman.f90 set_filename.F90 ecom1-mods.f90 zpline.f90 
   ecom2-mods.f90 curve2d_mod.f90 data_input.F90 set_init.f90
   response_matrix.F90 shapesurf.F90 setup_data_fetch.F90 current.f90
   external_current.F90 boundary.F90 spline.f90 pltdat.F90

efit/boundary.F90
@@ -50,7 +50,8 @@
       use error_control
       implicit integer*4 (i-n), real*8 (a-h, o-z)
       double precision, dimension(nwh) :: psi, zero
-      double precision, dimension(nwh) :: x,y
+      double precision, dimension(nw) :: x
+      double precision, dimension(nh) :: y
       double precision, dimension(npoint) :: xcontr,ycontr
       double precision, dimension(5) :: dist(5)
       double precision, dimension(limitr) :: xlim,ylim
@@ -1446,8 +1447,8 @@
       implicit integer*4 (i-n), real*8 (a-h,o-z)
 
       real*8, intent(out) :: xseps(2),yseps(2)
-      dimension x(nx),y(nz),pds(6),xxout(kfound),yyout(kfound),psipsi(nx*nz)
-      dimension bpoo(kfound),bpooz(kfound),pdss(6),xlimv(limtrv),ylimv(limtrv)
+      dimension x(nx),y(nz),pds(6),xxout(npoint),yyout(npoint),psipsi(nx*nz)
+      dimension bpoo(npoint),bpooz(npoint),pdss(6),xlimv(limtrv),ylimv(limtrv)
       dimension pdsold(6),zeross(1),xs(1),ys(1)
       character(len=80) :: strtmp
       parameter (psitol=1.0e-04_dp)

efit/current.f90
@@ -522,9 +522,8 @@
        if (.not.fixpp) then
         cratio=ipmhd(jtime)/tcurrt
         if(abs(ipmhd(jtime)).le.1.e-3_dp) cratio=1.0
-        cratio_ext = cratio * cratio_ext
-        cratiop_ext = cratio_ext
-        cratiof_ext = cratio_ext
+        cratiop_ext = cratio * cratiop_ext
+        cratiof_ext = cratio * cratiof_ext
         do kk=1,nwnh
           pcurrt(kk)=pcurrt(kk)*cratio
           pcurrtpp(kk)=pcurrtpp(kk)*cratio

commit 46a19b89f8cd179d1cf48777bd5c04aa5af841ad

Fix bug introduced by 55535cc0255af951439c87e2dafdeff4c0234419 and another in rand_knot_opt

efit/ecom2-mods.f90
@@ -57,7 +57,7 @@
       end module var_bscom
 
       module var_autoknot
-      integer*4 :: kautoknt,kakloop,kakiter
+      integer*4 :: kautoknt,kakloop,kakiter,lead_rank
       integer*4 :: ks_a,ktime_a,kerror_a,kadknt,mxiter_a,ttime
       real*8 :: akchiwt,akerrwt,aktol,akgamwt,akprewt
       real*8,dimension(:),allocatable :: aeeknt,awwknt,affknt,appknt

efit/print_terminal.F90
@@ -49,7 +49,7 @@
 !#if defined(USEMPI)
 !      ! TODO: Currently this ONLY works if nproc == total number of time slices.
 !      ! If running in parallel, write out key info for all ranks
-!      if (nproc > 1) then
+!      if (nproc>1) then
 !        if (rank==0) then
 !          allocate(ishotall(nproc))
 !          allocate(timeall(nproc))
@@ -75,7 +75,7 @@
 !#endif
 !      if ((ivacum.eq.0).and.(itek.le.0)) then
       if (ivacum.eq.0) then
-        mpi_rank: if (rank == 0) then
+        mpi_rank: if (rank==lead_rank) then
           !write (nttyo,10000) trim(ch1),trim(ch2)
           saisq=chisq(it)
           ! avoid roundoff
@@ -99,9 +99,7 @@
 ! --- delete fitout.dat if IOUT does not contain 1
 !
       if (iand(iout,1).eq.0) then ! if iout is even, including zero
-        mpi_rank0: if (rank == 0) then
-          close(unit=nout,status='delete')
-        endif mpi_rank0
+        if(rank==lead_rank) close(unit=nout,status='delete')
       else
         !write (nout,10000) trim(ch1),trim(ch2)
         saisq=chisq(it)
@@ -416,7 +414,7 @@
           enddo
         enddo
 !
-        if (rank == 0) then
+        if (rank==lead_rank) then
           call open_new(80,'mhdin.new','','quote')
           write (80,in3)
           close(unit=80)
@@ -680,7 +678,7 @@
       include 'modules1.inc'
 
       if (itek.le.0) then
-        if(rank == 0) write(nttyo,10001) nw,nh
+        if(rank==0) write(nttyo,10001) nw,nh
       endif
 
       ! if iout is odd

efit/rand_knot_opt.F90
@@ -25,67 +25,76 @@
       real*8, dimension(nproc) :: errall
       real*8, dimension(:), allocatable :: lbnd,krange,kpos,kbest
 
-      kerror = 0
+      kerror=0
+      loc=1
 
       ! Run with input settings first
       if(iconvr.lt.0) return
       call set_init(ks)
       call fit(ks,kerror)
       if(rank==0) write(6,*) 'Initial error is: ',terror(ks)
-      if ((kerror == 0) .and. (terror(ks).le.error)) then
-        if(rank==0) write(6,*) 'Input settings converged'
-        return
+      if ((kerror==0) .and. (terror(ks).le.error)) then
+        if (rank==0) then
+          write(6,*) 'Input settings converged'
+          return
+        else
+          ! No need for parallel processing
+#if defined(USEMPI)
+          call mpi_finalize(ierr)
+#endif
+          stop
+        endif
       endif
       berror=terror(ks)
 
       ! Setup bounds for varying each knot
       nvary=0
-      if(kppfnc .eq. 6) nvary=nvary+kppknt-2
-      if(kfffnc .eq. 6) nvary=nvary+kffknt-2
-      if(kwwfnc .eq. 6) nvary=nvary+kwwknt-2
-      if(keefnc .eq. 6) nvary=nvary+keeknt-2
+      if(kppfnc.eq.6) nvary=nvary+kppknt-2
+      if(kfffnc.eq.6) nvary=nvary+kffknt-2
+      if(kwwfnc.eq.6) nvary=nvary+kwwknt-2
+      if(keefnc.eq.6) nvary=nvary+keeknt-2
       if (nvary.le.0) then
         if(rank==0) write(6,*) 'No knots to vary'
         return
       endif
       allocate(lbnd(nvary),krange(nvary),kpos(nvary),kbest(nvary))
       j=0
-      if (kppfnc .eq. 6) then
+      if (kppfnc.eq.6) then
         do i=2,kppknt-1
           j=j+1
-          lbnd(j) = ppknt(i)-appdf(i)*(ppknt(i)-ppknt(i-1))
-          krange(j) = appdf(i)*(ppknt(i+1)-ppknt(i-1))
-          kbest(j) = ppknt(i)
+          lbnd(j)=ppknt(i)-appdf(i)*(ppknt(i)-ppknt(i-1))
+          krange(j)=appdf(i)*(ppknt(i+1)-ppknt(i-1))
+          kbest(j)=ppknt(i)
         enddo
       endif
-      if (kfffnc .eq. 6) then
+      if (kfffnc.eq.6) then
         do i=2,kffknt-1
           j=j+1
-          lbnd(j) = ffknt(i)-affdf(i)*(ffknt(i)-ffknt(i-1))
-          krange(j) = affdf(i)*(ffknt(i+1)-ffknt(i-1))
-          kbest(j) = ffknt(i)
+          lbnd(j)=ffknt(i)-affdf(i)*(ffknt(i)-ffknt(i-1))
+          krange(j)=affdf(i)*(ffknt(i+1)-ffknt(i-1))
+          kbest(j)=ffknt(i)
         enddo
       endif
-      if (kwwfnc .eq. 6) then
+      if (kwwfnc.eq.6) then
         do i=2,kwwknt-1
           j=j+1
-          lbnd(j) = wwknt(i)-awwdf(i)*(wwknt(i)-wwknt(i-1))
-          krange(j) = awwdf(i)*(wwknt(i+1)-wwknt(i-1))
-          kbest(j) = wwknt(i)
+          lbnd(j)=wwknt(i)-awwdf(i)*(wwknt(i)-wwknt(i-1))
+          krange(j)=awwdf(i)*(wwknt(i+1)-wwknt(i-1))
+          kbest(j)=wwknt(i)
         enddo
       endif
-      if (keefnc .eq. 6) then
+      if (keefnc.eq.6) then
         do i=2,keeknt-1
           j=j+1
-          lbnd(j) = eeknt(i)-aeedf(i)*(eeknt(i)-eeknt(i-1))
-          krange(j) = aeedf(i)*(eeknt(i+1)-eeknt(i-1))
-          kbest(j) = eeknt(i)
+          lbnd(j)=eeknt(i)-aeedf(i)*(eeknt(i)-eeknt(i-1))
+          krange(j)=aeedf(i)*(eeknt(i+1)-eeknt(i-1))
+          kbest(j)=eeknt(i)
         enddo
       endif
 
 #if defined(USEMPI)
-      if (nproc > 1) then
-        if (rank == 0 .and. ttime==1) then
+      if (nproc>1) then
+        if (rank==0 .and. ttime==1) then
           ! TODO: it should be possible to distribute processors effectively for multiple times and 
           !       leave others for knot optimization, but that logic hasn't been setup yet
           !       (only can parallelize over one or the other and times takes precedence)
@@ -101,10 +110,10 @@
         endif
         if (ttime==1) then
           ! This could be avoided if MPI is handled differently
-          if(nproc > kakloop) stop
+          if(nproc>kakloop) stop
           kloop=kakloop/nproc
           do k=1,mod(kakloop,nproc)
-            if(rank == k-1) kloop=kloop+1
+            if(rank==k-1) kloop=kloop+1
           enddo
         else
           kloop=kakloop
@@ -118,7 +127,7 @@
 
       ! Vary knots randomly until convergence or max iterations is reached
       do k=1,kloop
-        kerror = 0
+        kerror=0
 
         ! With a bit of careful debugging we should be able to avoid this...
         call data_input(ks,ktime,kerror)
@@ -131,41 +140,41 @@
         call random_number(kpos)
         kpos=lbnd+kpos*krange
         j=0
-        if (kppfnc .eq. 6) then
+        if (kppfnc.eq.6) then
           do i=2,kppknt-1
             j=j+1
-            ppknt(i) = kpos(j)
+            ppknt(i)=kpos(j)
           enddo
         endif
-        if (kfffnc .eq. 6) then
+        if (kfffnc.eq.6) then
           do i=2,kffknt-1
             j=j+1
-            ffknt(i) = kpos(j)
+            ffknt(i)=kpos(j)
           enddo
         endif
-        if (kwwfnc .eq. 6) then
+        if (kwwfnc.eq.6) then
           do i=2,kwwknt-1
             j=j+1
-            wwknt(i) = kpos(j)
+            wwknt(i)=kpos(j)
           enddo
         endif
-        if (keefnc .eq. 6) then
+        if (keefnc.eq.6) then
           do i=2,keeknt-1
             j=j+1
-            eeknt(i) = kpos(j)
+            eeknt(i)=kpos(j)
           enddo
         endif
 
         ! Run fit
-        mxiter = kakiter
+        mxiter=kakiter
         call set_init(ks)
         call fit(ks,kerror)
-        if(kerror .ne. 0) terror(ks)=999.
+        if(kerror.ne.0) terror(ks)=999.
         write(6,*) 'Iteration ',(k-1)*nproc+rank+1,' has error: ',terror(ks)
 
 #if defined(USEMPI)
         ! Get best error and corresponding knot positions from all processors
-        if (nproc > 1 .and. ttime==1) then
+        if (nproc>1 .and. ttime==1) then
           errall=999.
           call MPI_GATHER(terror(ks),1,MPI_DOUBLE_PRECISION,errall,1,MPI_DOUBLE_PRECISION,0,MPI_COMM_WORLD,ierr)
           if(rank==0) loc=minloc(errall)
@@ -177,7 +186,8 @@
 
         ! Check convergence and save best error and knots
         if (terror(ks).le.error) then
-          if (rank == 0 .or. ttime>1) then
+          lead_rank=loc(1)-1
+          if (rank==lead_rank .or. ttime>1) then
             write(6,*) 'New knot locations converged'
             return
           else
@@ -211,28 +221,28 @@
         return
       endif
       j=0
-      if (kppfnc .eq. 6) then
+      if (kppfnc.eq.6) then
         do i=2,kppknt-1
           j=j+1
-          ppknt(i) = kbest(j)
+          ppknt(i)=kbest(j)
         enddo
       endif
-      if (kfffnc .eq. 6) then
+      if (kfffnc.eq.6) then
         do i=2,kffknt-1
           j=j+1
-          ffknt(i) = kbest(j)
+          ffknt(i)=kbest(j)
         enddo
       endif
-      if (kwwfnc .eq. 6) then
+      if (kwwfnc.eq.6) then
         do i=2,kwwknt-1
           j=j+1
-          wwknt(i) = kbest(j)
+          wwknt(i)=kbest(j)
         enddo
       endif
-      if (keefnc .eq. 6) then
+      if (keefnc.eq.6) then
         do i=2,keeknt-1
           j=j+1
-          eeknt(i) = kbest(j)
+          eeknt(i)=kbest(j)
         enddo
       endif
       call set_init(ks)

efit/set_defaults.f90
@@ -95,6 +95,7 @@
       kwwfnc=0
       kwwknt=0
       kzeroj=0
+      lead_rank=0
       limid=33
       lring=0
       msebkp=0

efit/tables.F90
@@ -54,11 +54,11 @@
         input_dir=trim(link_efit)
       endif
       if (link_store(1:1).ne.'')  store_dir=trim(link_store)
-      table_di2 = table_dir
+      table_di2=table_dir
 
       if (rank==0) then
         ! get the files
-        call system('ls '//trim(table_di2)//' > shot_tables.txt')
+        call system('ls '//trim(table_dir)//' > shot_tables.txt')
         open(31,FILE='shot_tables.txt',action="read")
 
         !how many
@@ -90,7 +90,7 @@
         ! set the table directory
         do i=1, nfiles
           if(ishot.ge.shot_tables(i)) &
-            table_di2 = trim(table_di2)//trim(filenames(i))//'/'
+            table_di2 = trim(table_dir)//trim(filenames(i))//'/'
         enddo
         ltbdi2 = len(trim(table_di2))
         write(nttyo,10000) trim(table_di2)